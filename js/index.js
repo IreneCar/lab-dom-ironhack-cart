b315878c3efe0eb4a18a5f81a6f8bc20762785ce
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-06-07 17:18:55 +0200

    iteration 1 resuelta

--- js/index.js
+++ js/index.js
@@ -2,15 +2,29 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+ //declaro una variable que dice donde trabajamos del html.
+  let priceElem = product.querySelector(".price span");
+ //declaro variable  que recoja el texto escrito en el html
+  let priceNum = priceElem.innerHTML;
+ // selecciono de donde quiero extraer los datos
+  let quantityElem = product.querySelector(".quantity input");
+ //declaro variable que coja los datos del value del input.
+  let quantityNum = quantityElem.value;
+ // declaro funcion que diga donde quiero actuar del html
+  let subtotalElem = product.querySelector(".subtotal span");
+ // declaro variable que recoja el texto del html tanto del price como del quantity y los multiplique
+  let subtotalNum = priceNum * quantityNum;
+ //digo que el texto del subtotal que tenemos en el html se sustituya por el que se da en subtotalNum
+  subtotalElem.innerHTML = subtotalNum;
+  return subtotalNum;
+ 
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  const singleProductElem = document.querySelector('.product');
+  updateSubtotal(singleProductElem);
   // end of test
 
   // ITERATION 2
