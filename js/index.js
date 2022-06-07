a14d9e905d34d91b783f135ed6102c3bbba86435
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-06-07 20:31:46 +0200

    iteration 2 and 3 complete

--- js/index.js
+++ js/index.js
@@ -21,17 +21,31 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
+  /*code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProductElem = document.querySelector('.product');
-  updateSubtotal(singleProductElem);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  //Declaramos vairable con el total de subtotalNum y la vamos a buscar al html (cajita:)
+
+  --> let totalElement = document.querySelector("#total-value span");
+
+  // escribimos dentro de la cajita: el subtotal 
+
+  --> totalElement.innerText = allSubtotalNum;
+  
+  // end of test*/
+
+ // ITERATION 2
+ let total = 0;
+ let allProductsElem = document.querySelectorAll(".product");
+ for (productos of allProductsElem){
+   total += updateSubtotal(productos);
+ }
+
+  
 
   // ITERATION 3
-  //... your code goes here
+  let totalElement = document.querySelector("#total-value span");
+  totalElement.innerText = total;
 }
 
 // ITERATION 4
